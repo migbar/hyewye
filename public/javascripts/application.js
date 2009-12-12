5252b183f3557cd42ae7732d2320a31f426981f6
Miguel Barcos <miguel@galar.com>
2009-12-12 17:02:44 -0500

    added scrolling to events stream in home page

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,20 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+var delay = 6000;
+var count = 30;
+var showing = 10;
+var speed = 700;
+var i = 0;
+function move(i) {
+  return function() {
+    $('#event_'+i).remove().css('display', 'none').prependTo('#events-list');
+  }
+}
+function scrollEvents() {
+  var toShow = (i + showing) % count;
+  $('#event_'+toShow).slideDown(speed, move(i));
+  $('#event_'+i).slideUp(speed, move(i));
+  i = (i + 1) % count;
+  setTimeout('scrollEvents()', delay);
+}    
+$(document).ready(function() {
+  setTimeout('scrollEvents()', delay);
+});
